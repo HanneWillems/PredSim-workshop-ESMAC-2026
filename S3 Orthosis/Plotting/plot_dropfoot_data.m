a6e14c76eb74d0b365e571fdfb2642450b360559
Tim travel <timvdzee@hotmail.com>
2026-09-01 12:55:48 +0200

    update figures

--- S3 Orthosis/Plotting/plot_dropfoot_data.m
+++ S3 Orthosis/Plotting/plot_dropfoot_data.m
@@ -9,5 +9,5 @@ for j = 1:length(joints)
     data = csvread(['dropfoot_', joints{j}, '.csv']);
 
     subplot(2,3,j+3)
-    plot(data(:,1), sgns(j)*data(:,2),'k--','linewidth', 2, 'DisplayName', 'Dropfoot')
+    plot(data(:,1), sgns(j)*data(:,2),'k--','linewidth', 2, 'DisplayName', 'Data (dropfoot)')
 end
